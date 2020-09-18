33abec43d52e7a97a6bec79a34b30529a10d210e
ONOGI Shinya <shinya.onogi.1979@ieee.org>
2020-09-18 19:16:06 +0900

    Add project files.

---
+++ ndiTracker/ndiTracker.cpp
@@ -0,0 +1,12 @@
+﻿// ndiTracker.cpp : Defines the entry point for the application.
+//
+
+#include "ndiTracker.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
